edf8201de1b7182fc1c1185ec3e244998357db02
RAULPC\rgr_1 <rgr182>
2024-04-17 23:32:21 -0600

    S3 temporal update please squash this

---
+++ VC_API/Models/PetVM.cs
@@ -0,0 +1,14 @@
+﻿namespace VC_API.Models
+{
+    public class PetVM
+    {       
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Color { get; set; }
+        public char? Gender { get; set; }
+        public string? Address { get; set; }
+        public byte[] Image { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
