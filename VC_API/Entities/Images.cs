e6f2a0284752220de19720978e101e565bd518b3
Raul <raul12141@gmail.com>
2024-05-22 10:59:40 -0600

    bjhz
    ZCnKJZC

--- VC_API/Entities/Images.cs
+++ VC_API/Entities/Images.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.CSharp;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VC_API.Entities
 {
+    [Keyless]
     public class Images
     {
+        
         public int PetId {  get; set; }
+        [NotMapped]
         public required IFormFile File { get; set; }
     }
 }
